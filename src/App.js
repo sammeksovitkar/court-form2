65dc3e0427d3c8b647502dc541c0dc6e58025cb0
sammeksovitkar <83156211+sammeksovitkar@users.noreply.github.com>
2026-09-04 15:42:57 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -212,7 +212,7 @@ const LoginPage = ({ onLogin }) => {
                 <div className="p-8 bg-indigo-700 text-white text-center rounded-t-3xl">
                     <Gavel className="mx-auto mb-3 text-teal-400" size={48} />
                     <h1 className="text-3xl font-extrabold tracking-wider">न्यायालयीन प्रणाली</h1>
-                    <p className="text-indigo-200 mt-1">वॉरंट / समन्स / नोटीस तयार करणे </p>
+                    <p className="text-indigo-200 mt-1">वॉरंट / समन्स / नोटीस / जंत्री तयार करणे </p>
                 </div>
                 
                 <form onSubmit={handleLogin} className="p-8 space-y-6">
